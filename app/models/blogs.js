b9cec62ba64c838f054d5f785cec8045b890c76f
navid8181 <nr5391894@gmail.com>
2023-07-11 19:17:13 +0330

    set up blogs schema

--- app/models/blogs.js
+++ app/models/blogs.js
@@ -2,12 +2,22 @@ const { default: mongoose } = require("mongoose");
 
 const schema = new mongoose.Schema({
 
+    author : {type : mongoose.Types.ObjectId,required : true},
+    title : {type : String,required : true},
+    text : {type : String,required : true},
+    image : {type : String,required : true},
+    tags : {type : [String],default : []},
+    category: {type : mongoose.Types.ObjectId,required : true},
+    comment : {type : [],default : []},
+    like : {type : [mongoose.Types.ObjectId],default : []},
+    dislike : {type : [mongoose.Types.ObjectId],default : []},
+    bookmark : {type : [mongoose.Types.ObjectId],default : []}
 
 })
 
 
 module.exports = {
 
-    BlogModel : mongoose.model("",schema)
+    BlogModel : mongoose.model("blog",schema)
 
 }
